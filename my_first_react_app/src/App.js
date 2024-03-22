1c7e6ad426ff00afe59bd41f80983c6c3be78c7e
burhan1997 <burhanelaldi97@gmail.com>
2024-03-22 14:40:14 +0100

    api is added to movie app

--- my_first_react_app/src/App.js
+++ my_first_react_app/src/App.js
@@ -1,9 +1,63 @@
-import React from "react";
+import {useEffect} from "react";
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = "http://www.omdbapi.com?apikey=ce1ea4dd";
+
+const movie1 = {
+  Title: "Fighting, Flying and Driving: The Stunts of Spiderman 3",
+  Year: "2007",
+  imdbID: "tt1132238",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg",
+};
 
 const App = () => {
-  return (
-    <h1>App</h1>
-  );
+
+    const searchMovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+
+      console.log(data.Search);
+    }
+
+    useEffect(() => {
+      searchMovies('Spiderman');
+    }, []);
+
+
+    return (
+      <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search">
+          <input
+            placeholder="Search for movies"
+            value="Superman"
+            onChange={() => {}}
+          />
+          <img src={SearchIcon} alt="search" onClick={() => {}} />
+        </div>
+
+        <div className="container">
+          <div className="movie">
+            <div>
+              <p>{movie1.Year}</p>
+            </div>
+            <div>
+              <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+            </div>
+
+            <div>
+              <span>{movie1.Type}</span>
+              <h3>{movie1.Title}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 
